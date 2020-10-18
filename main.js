ce0d916234cd014771413655ed07a8c1f1b0b4cb
ROHITHstar <71971571+ROHITHstar@users.noreply.github.com>
2020-10-18 00:52:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -55,5 +55,29 @@ client.on('message', message => {
 
 
 
+client.on('message',msg => {
+if (message.content === '+say'){
+ const args = message.content.split(' ');
+        const command = args.shift().toLowerCase();
+        message.delete();
 
+        if (!message.member.hasPermission("MANAGE_MESSAGES"))
+            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
+
+        if (args.length < 0)
+            return message.reply("Nothing to say?").then(m => m.delete(5000));
+
+        
+
+        if (args[0].toLowerCase() === "announce") {
+            const embed = new MessageEmbed()
+                .setTitle("Announcement ")
+                .setDescription(`${args.slice(1).join(" ")}`)
+                .setColor("#00ff00");
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+}
 client.login(process.env.token);
