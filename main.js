c5779a32392beb150e9b244038618d13ef9caf66
ROHITHstar <71971571+ROHITHstar@users.noreply.github.com>
2020-10-28 20:06:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,6 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
-		console.log(`${client.user.tag} is now active!`)
-	  
-		client.user.setActivity('119 members', ({type: "WATCHING"}))
-	  })
 
 client.on('message', message => {
    if (message.content === 'Hi') {
